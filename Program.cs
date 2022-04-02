8ae2e97a9e103031903ee41c92cde80e97741652
OlekanmaVictoria <olekanmavictoria1994@gmail.com>
2022-04-02 12:49:09 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, my name is olekanma victoria, I am in backend wellahealthschool!");
+Console.WriteLine("because  I love logic and working behind the scene in getting the work done");
+
